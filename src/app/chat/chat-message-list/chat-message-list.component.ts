f955e2cb7f865b4264339d601019a12c9a2225ac
Ertan Kara <kara.ertan9@gmail.com>
2024-01-27 00:10:13 +0300

    feat: match requirements in Notion -> remove test data, make the chat functional

--- src/app/chat/chat-message-list/chat-message-list.component.ts
+++ src/app/chat/chat-message-list/chat-message-list.component.ts
@@ -10,20 +10,7 @@ export class ChatMessageListComponent {
   private _messages: Message[] = [];
 
   constructor() {
-    this._messages = [
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=2', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=3', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=4', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=5', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'John Doe', avatar: 'https://picsum.photos/80/80?random=6', date: '2024-01-26', message: 'Good morning!' },
-      { name: 'Jane Doe', avatar: 'https://picsum.photos/80/80?random=7', date: '2024-01-26', message: 'Good morning, what a lovely day!' },
-      { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=8', date: '2024-01-26', message: 'Indeed :)' },
-      { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=9', date: '2024-01-26', message: 'Indeed :)' },
-      { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=10', date: '2024-01-26', message: 'Indeed :)' },
-      { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=11', date: '2024-01-26', message: 'Indeed :)' },
-      { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=12', date: '2024-01-26', message: 'Indeed :)' },
-    ]
+    this._messages = []
   }
 
   public get messages() {
@@ -33,32 +20,15 @@ export class ChatMessageListComponent {
   onMessage(newMessage: string) {
     // TODO: Send message to Firebase collection
     console.log('new message', newMessage);
+    this._messages.push({
+      name: 'Ertan Kara',
+      avatar: 'https://picsum.photos/80/80?random=2',
+      date: new Date().toISOString(),
+      message: newMessage,
+    })
   }
 
   loadMessages(...arg: any) {
-    setTimeout(() => {
-      this._messages.push(...[
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-        { name: 'Mike Doe', avatar: 'https://picsum.photos/80/80?random=1', date: '2024-01-26', message: 'Indeed :)' },
-      ]);
-    }, 200);
+    // Load older messages
   }
 }
