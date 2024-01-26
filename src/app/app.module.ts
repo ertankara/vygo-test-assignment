ddcdbb44605d8bac6e362ec1f0a4ec346480c916
Ertan Kara <kara.ertan9@gmail.com>
2024-01-27 01:00:52 +0300

    feat: add angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"vygo-test-assignment-ertan","appId":"1:314977826606:web:041cf7320f4bb8aafc2cbf","storageBucket":"vygo-test-assignment-ertan.appspot.com","apiKey":"AIzaSyBMQw_V9ThDvr8ZHUdj9I7MMVyg7f14t9s","authDomain":"vygo-test-assignment-ertan.firebaseapp.com","messagingSenderId":"314977826606"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
