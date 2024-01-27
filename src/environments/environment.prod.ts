1ee3d8da36d0dad84a17edccf205de8cf7bc71f4
Ertan Kara <kara.ertan9@gmail.com>
2024-01-27 05:07:19 +0300

    fix: update env var for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: "vygo-test-assignment-ertan",
+    appId: "1:314977826606:web:041cf7320f4bb8aafc2cbf",
+    storageBucket: "vygo-test-assignment-ertan.appspot.com",
+    apiKey: "AIzaSyBMQw_V9ThDvr8ZHUdj9I7MMVyg7f14t9s",
+    authDomain: "vygo-test-assignment-ertan.firebaseapp.com",
+    messagingSenderId: "314977826606"
+  }
 };
